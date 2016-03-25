6520c0cbdc62854e3ddb21dcf04603b458340af1
dl1ksv <dl1ksv@gmx.de>
2016-03-25 18:33:38 +0100

    Starting to use cmake modules from gnuradio directly

---
+++ cmake/Modules/FindGnuradioModules.cmake
@@ -0,0 +1,11 @@
+# - Try to find the cmake/Modules directory of an installed gnuradio
+# - This might be not the standard location
+# - Look for GrVersion.cmake
+
+find_path(GNURADIO_MODULE_DIRECTORY
+	NAMES GrVersion.cmake
+	HINTS ${CMAKE_INSTALL_PREFIX}/lib/cmake/gnuradio
+	PATHS
+          /usr/lib64/cmake/gnuradio
+          /usr/lib/cmake/gnuradio
+)
