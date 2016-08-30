eaeb3cf0f3ce69aad7dc63cd9fc06d06b3a129c5
dl1ksv <dl1ksv@gmx.de>
2016-08-30 22:22:28 +0200

    Adding lib64 to path in hints

--- cmake/Modules/FindGnuradioModules.cmake
+++ cmake/Modules/FindGnuradioModules.cmake
@@ -4,7 +4,7 @@
 
 find_path(GNURADIO_MODULE_DIRECTORY
 	NAMES GrVersion.cmake
-	HINTS ${CMAKE_INSTALL_PREFIX}/lib/cmake/gnuradio
+	HINTS ${CMAKE_INSTALL_PREFIX}/lib/cmake/gnuradio ${CMAKE_INSTALL_PREFIX}/lib64/cmake/gnuradio
 	PATHS
           /usr/lib64/cmake/gnuradio
           /usr/lib/cmake/gnuradio
